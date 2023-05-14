36243bda6eb7920d956d3b7ace1d9080295d9773
John Monk <johntmonk1@gmail.com>
2023-05-13 21:16:08 -0500

    required file system and path modules, and express.static middleware function for serving images files in public directory associated with json data

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.static('public'));
 
 let db,
     dbConnectionStr = '',
